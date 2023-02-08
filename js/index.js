443bc4a825955690c78f688f569afe7e370a3a4e
aritos20 <agianicopulos@gmail.com>
2023-02-08 17:26:38 +0100

    little problem with the commit do it before this one

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+const xMenu = document.querySelector('.header__x-menu');
+const dropDownMenu = document.querySelector('.header__dropdown-menu');
+const burgerMenu = document.querySelector('.header__burger-menu');
+
+burgerMenu.addEventListener('click', () => {
+  xMenu.classList.toggle('active');
+  dropDownMenu.classList.toggle('active');
+  burgerMenu.classList.toggle('active');
+})
+
+xMenu.addEventListener('click', () => {
+  xMenu.classList.toggle('active');
+  dropDownMenu.classList.toggle('active');
+  burgerMenu.classList.toggle('active');
+})
+
+
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
