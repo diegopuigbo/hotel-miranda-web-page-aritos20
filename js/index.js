0683075beba6cefb37240a57be48c7f050bb5c91
aritos20 <agianicopulos@gmail.com>
2023-02-10 13:41:09 +0100

    features slider of the home page is done

--- js/index.js
+++ js/index.js
@@ -6,34 +6,30 @@ burgerMenu.addEventListener('click', () => {
   xMenu.classList.toggle('active');
   dropDownMenu.classList.toggle('active');
   burgerMenu.classList.toggle('active');
-})
+});
 
 xMenu.addEventListener('click', () => {
   xMenu.classList.toggle('active');
   dropDownMenu.classList.toggle('active');
   burgerMenu.classList.toggle('active');
-})
+});
 
-
-
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.rooms__swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
   
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+  });
+
+const swiperFacilities = new Swiper(".facilities__swiper", {
+    loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
     },
   });
